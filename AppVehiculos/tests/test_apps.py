df9f751af84cd67e63fb5ecce38ff0f22b82b962
migueltovar <loganitsolutions@outlook.com>
2025-04-04 00:02:33 -0500

    Corregir nombre de la aplicación en la prueba de configuración de AppVehiculos

--- AppVehiculos/tests/test_apps.py
+++ AppVehiculos/tests/test_apps.py
@@ -5,5 +5,5 @@ from AppVehiculos.apps import AppvehiculosConfig
 
 class AppVehiculosConfigTest(TestCase):
     def test_app_config(self):
-        self.assertEqual(AppvehiculosConfig.name, "AppVehixculos")
+        self.assertEqual(AppvehiculosConfig.name, "AppVehiculos")
         self.assertEqual(apps.get_app_config("AppVehiculos").name, "AppVehiculos")
